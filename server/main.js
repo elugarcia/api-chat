ae6bc8ea86023746aee8f798783a152ab87a0d5b
elianagarcia5 <elianagarcia@aitsolutions.com.ar>
2023-01-24 10:43:51 -0300

    first commit

---
+++ server/main.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
+
+var messages = [
+  {
+    id: 1,
+    text: 'Hola!',
+    author: 'Lionel'
+  },
+  {
+    id: 2,
+    text: 'Cómo están??',
+    author: 'Emiliano'
+  }
+];
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/hello', function (req, res) {
+  res.status(200).send('Hello World!');
+});
+
+io.on('connection', function (socket) {
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
+
+  socket.on('new-message', function (data) {
+    messages.push(data);
+
+    io.sockets.emit('messages', messages);
+  });
+});
+
+server.listen(8080, function () {
+  console.log('Servidor corriendo en http://localhost:8080');
+});
